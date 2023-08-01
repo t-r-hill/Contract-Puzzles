4531be635f59be3420865f67bd1224db22ae90bc
Thomas Hill <tomhill555@gmail.com>
2023-08-01 18:58:56 +0100

    completed games 4 and 5

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,14 +6,17 @@ describe('Game4', function () {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const [signer] = await ethers.getSigners(0);
+
+    return { game, signer };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
+    await game.write(signer.getAddress());
 
-    await game.win();
+    await game.win(signer.getAddress());
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
--- test/game5Test.js
+++ test/game5Test.js
@@ -6,14 +6,39 @@ describe('Game5', function () {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    const deployer = await ethers.provider.getSigner(0);
+
+    return { game, deployer };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, deployer } = await loadFixture(deployContractAndSetVariables);
+    const threshhold = '0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf';
+    let address = '';
+    let etherToSend = ethers.utils.parseEther("1.0")
 
     // good luck
-
-    await game.win();
+    do {
+      let signer = await ethers.Wallet.createRandom();
+      address = await signer.getAddress();
+      console.log(address);
+      if (address < threshhold){
+        // send ether to address
+        console.log('Deployer address:', await deployer.getAddress());
+        console.log('Deployer balance:', await deployer.getBalance());
+        console.log('Sending tx:', address, etherToSend);
+        tx = await deployer.sendTransaction({
+          value: etherToSend,
+          to: address
+        })
+        await tx.wait();
+        console.log('Deployer address:', await deployer.getAddress());
+        console.log('Deployer balance:', await deployer.getBalance());
+        signer = await signer.connect(ethers.provider);
+        console.log('Signer address:', await signer.getAddress());
+        console.log('Signer balance:', await signer.getBalance());
+        await game.connect(signer).win();
+      }
+    } while (address >= threshhold);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
